c46157166ead5fb6f56c37f1c0e176b81f29d57c
Rio's <kj.ritto@live.in>
2025-02-20 20:25:52 +0530

    android ejected

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,12 @@
 import axios from 'axios';
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, {
+	useState,
+	useEffect,
+	useRef,
+	useCallback,
+	useMemo,
+} from 'react';
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import moment from 'moment-timezone';
 
@@ -8,282 +14,6 @@ const headers = {
 	'Content-Type': 'application/json',
 	'X-Api-Key': process.env.EXPO_PUBLIC_API_KEY,
 };
-
-const getCurrentTimeSpan = () => {
-	return moment().tz('Asia/Dubai').format('HH:mm') + ':00'; // "HH:mm:ss"
-};
-
-export default function App() {
-	const [deviceId, setDeviceId] = useState<string | null>(null);
-	const [connected, setConnected] = useState(false);
-	const [defaultScreen, setDefaultScreen] = useState(false);
-	const [error, setError] = useState(false);
-	const [doctorData, setDoctorData] = useState(null);
-	const [hospitalDetails, setHospitalDetails] = useState(null);
-	const deviceIdRef = useRef(null);
-	useEffect(() => {
-		const generateDeviceId = () => {
-			// Generate random 8 digit number
-			const min = 100000; // Smallest 8 digit number
-			const max = 999999; // Largest 8 digit number
-			const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-			return randomNum.toString();
-		};
-		const getDeviceId = async () => {
-			try {
-				let result = await SecureStore.getItemAsync('deviceId');
-				let hospital = await SecureStore.getItemAsync('hospitalDetails');
-				if (hospital) setHospitalDetails(JSON.parse(hospital));
-				const id = result || generateDeviceId();
-				if (!result) await SecureStore.setItemAsync('deviceId', id);
-				setDeviceId(id);
-				deviceIdRef.current = id;
-				fetchData();
-				//Send device ID to backend here (e.g., using fetch or axios)
-			} catch (e) {}
-		};
-		getDeviceId();
-	}, []);
-
-	const fetchData = async () => {
-		try {
-			let dId = deviceIdRef.current;
-			let hospital = await SecureStore.getItemAsync('hospitalDetails');
-			const url = `${
-				process.env.EXPO_PUBLIC_BASE_URL
-			}?deviceCode=${dId}&currentTime=${getCurrentTimeSpan()}`;
-			const { data, status } = await axios.get(url, { headers }); // Replace with your API endpoint
-			if (status !== 200) {
-				setError(true);
-				throw new Error(`HTTP error! status: ${status}`);
-			} else {
-				if (data.scheduleStatus === 1) {
-					setConnected(true);
-					setDefaultScreen(false);
-				} else if (data.scheduleStatus === 2) {
-					setConnected(false);
-					setDoctorData(null);
-					setDefaultScreen(false);
-				} else if (data.scheduleStatus === 3) {
-					setConnected(false);
-					setDoctorData(null);
-					setDefaultScreen(true);
-					setError(true);
-				} else if (data.scheduleStatus === 4) {
-					setConnected(false);
-					setDefaultScreen(false);
-					setDoctorData(null);
-					setError(true);
-				}
-			}
-			if (!hospital || JSON.stringify(data) !== JSON.stringify(hospital)) {
-				await SecureStore.setItemAsync(
-					'hospitalDetails',
-					JSON.stringify({
-						helpEmail: data?.helpEmail || '',
-						helpPhone: data?.helpPhone || '',
-						hospitalName: data?.hospitalName || '',
-						bgColour: data?.bgColour || '',
-						hospitalWebSite: data?.hospitalWebSite || '',
-						roomName: data?.roomName || '',
-					})
-				);
-				setDoctorData(data);
-			}
-			setError(false);
-		} catch (err) {
-			setError(err);
-		}
-	};
-
-	useEffect(() => {
-		if (deviceId) {
-			let intervalId;
-			intervalId = setInterval(fetchData, 1 * 60 * 1000); // Retry every 1 minute if no data
-			return () => clearInterval(intervalId); // Clear interval on unmount
-		}
-	}, [deviceId]);
-
-	if (defaultScreen) {
-		return (
-			<View style={styles.defaultImageContainer}>
-				<Image
-					source={require('../assets/images/default.jpeg')}
-					style={styles.defaultImage}></Image>
-			</View>
-		);
-	}
-
-	if (!connected) {
-		return (
-			<View style={styles.centralizedContainer}>
-				<Text
-					style={[
-						styles.optSubtitle,
-						{ position: 'absolute', top: 40, left: 40 },
-					]}>
-					{hospitalDetails?.hospitalName}
-				</Text>
-				<Text style={styles.optTitle}>Enter code</Text>
-				<Text style={styles.optSubtitle}>
-					Enter this code in the back office to connect the device
-				</Text>
-				<View style={styles.codeContainer}>
-					{deviceId?.split('').map((digit, index) => (
-						<View
-							key={index}
-							style={styles.digitBox}>
-							<Text style={styles.digit}>{digit}</Text>
-						</View>
-					))}
-				</View>
-			</View>
-		);
-	}
-
-	if (error && !doctorData?.doctorName) {
-		return (
-			<View style={styles.errorContainer}>
-				<Text
-					style={[
-						styles.optSubtitle,
-						{ position: 'absolute', top: 40, left: 40 },
-					]}>
-					{hospitalDetails?.hospitalName}
-				</Text>
-				<Text style={styles.optTitle}>Internal Server Error</Text>
-				<Text
-					style={{
-						fontSize: 24,
-						marginBottom: 16,
-						fontFamily: 'InterRegular',
-						color: '#fff',
-					}}>
-					Please Contact Administrator
-				</Text>
-				<Text
-					style={{
-						fontSize: 18,
-						marginBottom: 16,
-						fontFamily: 'InterLight',
-						color: '#fff',
-					}}>
-					{`Email: ${hospitalDetails?.helpEmail || ''} | Phone: ${
-						hospitalDetails?.helpPhone || ''
-					}`}
-				</Text>
-			</View>
-		);
-	}
-
-	if (!doctorData?.doctorName) {
-		return (
-			<View style={styles.centralizedContainer}>
-				<Text
-					style={[
-						styles.optSubtitle,
-						{ position: 'absolute', top: 40, left: 40 },
-					]}>
-					{hospitalDetails?.hospitalName}
-				</Text>
-				<Text style={styles.optTitle}>Screen is empty</Text>
-			</View>
-		);
-	}
-	return (
-		<View style={styles.container}>
-			<View
-				style={[
-					styles.leftSection,
-					{ backgroundColor: doctorData?.bgColor || '#0090FF' },
-				]}>
-				<Image
-					source={require('../assets/images/line.png')}
-					style={styles.bgImage}></Image>
-				<View
-					style={{
-						flex: 1,
-						flexDirection: 'column',
-						justifyContent: 'space-between',
-						padding: 40,
-						paddingRight: 60,
-					}}>
-					<View>
-						<Text style={styles.lightText}>
-							{hospitalDetails?.hospitalName}
-						</Text>
-					</View>
-					<View>
-						<Text
-							style={{
-								fontSize: 48,
-								fontWeight: 'bold',
-								fontFamily: 'InterBold',
-								color: '#fff',
-								paddingBottom: 10,
-							}}>
-							{doctorData.doctorName}
-						</Text>
-						<Text style={[styles.lightText, { paddingBottom: 20 }]}>
-							{doctorData.department}
-						</Text>
-						<View
-							style={{
-								flexDirection: 'row',
-								borderTopColor: '#3FD2FF',
-								borderTopWidth: 1,
-								alignItems: 'center',
-							}}>
-							<View
-								style={{
-									backgroundColor: '#3FD2FF',
-									paddingHorizontal: 20,
-									paddingVertical: 10,
-									justifyContent: 'center',
-								}}>
-								<Text
-									style={{
-										color: '#fff',
-										fontFamily: 'Inter',
-										textAlign: 'center',
-										fontSize: 24,
-									}}>
-									Doctor in
-								</Text>
-							</View>
-							<Text
-								style={{
-									color: '#fff',
-									fontFamily: 'Inter',
-									textAlign: 'right',
-									flex: 1,
-									fontSize: 24,
-								}}>
-								{doctorData.timing}
-							</Text>
-						</View>
-						<Text
-							style={{
-								fontFamily: 'Inter',
-								color: '#fff',
-								paddingTop: 30,
-								lineHeight: 24,
-								letterSpacing: 0.5,
-							}}>
-							{hospitalDetails?.hospitalWebSite || ''}
-						</Text>
-					</View>
-				</View>
-			</View>
-			<View style={styles.rightSection}>
-				<Image
-					source={{ uri: doctorData?.photoUrl || '' }}
-					style={styles.image}
-				/>
-			</View>
-		</View>
-	);
-}
 
 const styles = StyleSheet.create({
 	container: {
@@ -400,3 +130,360 @@ const styles = StyleSheet.create({
 		color: '#fff',
 	},
 });
+
+const getContainerStyle = (bgColor?: string) => ({
+	backgroundColor: bgColor || '#0090FF',
+});
+
+interface DoctorData {
+	doctorName?: string;
+	department?: string;
+	timing?: string;
+	photoUrl?: string;
+	bgColor?: string;
+	scheduleStatus?: number;
+}
+
+interface HospitalDetails {
+	hospitalName?: string;
+	helpEmail?: string;
+	helpPhone?: string;
+	hospitalWebSite?: string;
+}
+
+export default function App() {
+	const [appState, setAppState] = useState({
+		connected: false,
+		defaultScreen: false,
+		error: false,
+	});
+
+	const [deviceId, setDeviceId] = useState<string | null>(null);
+	const [doctorData, setDoctorData] = useState<DoctorData | null>(null);
+	const [hospitalDetails, setHospitalDetails] =
+		useState<HospitalDetails | null>(null);
+
+	const intervalRef = useRef<NodeJS.Timeout>();
+	const deviceIdRef = useRef<string | null>(null);
+	const abortControllerRef = useRef<AbortController>();
+
+	const timeFormat = useMemo(() => 'HH:mm', []);
+
+	const getCurrentTimeSpan = useCallback(() => {
+		return moment().tz('Asia/Dubai').format(timeFormat) + ':00';
+	}, [timeFormat]);
+
+	const fetchData = useCallback(async () => {
+		if (abortControllerRef.current) {
+			abortControllerRef.current.abort();
+		}
+		abortControllerRef.current = new AbortController();
+
+		try {
+			const dId = deviceIdRef.current;
+			if (!dId) {
+				console.log('No device ID found');
+				return;
+			}
+
+			const hospital = await SecureStore.getItemAsync('hospitalDetails');
+			const currentTime = getCurrentTimeSpan();
+			const url = `${process.env.EXPO_PUBLIC_BASE_URL}?deviceCode=${dId}&currentTime=${currentTime}`;
+
+			const response = await axios.get(url, {
+				headers,
+				signal: abortControllerRef.current.signal,
+			});
+
+			if (response.status !== 200) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+
+			const { data, status } = response;
+
+			setAppState((prev) => {
+				const newState = {
+					...prev,
+					connected: data.scheduleStatus === 1,
+					defaultScreen: data.scheduleStatus === 3,
+					error: data.scheduleStatus === 3 || data.scheduleStatus === 4,
+				};
+				return newState;
+			});
+
+			setDoctorData(data.scheduleStatus === 1 ? data : null);
+
+			if (!hospital || JSON.stringify(data) !== hospital) {
+				const newHospitalDetails: HospitalDetails = {
+					helpEmail: data?.helpEmail || '',
+					helpPhone: data?.helpPhone || '',
+					hospitalName: data?.hospitalName || '',
+					hospitalWebSite: data?.hospitalWebSite || '',
+				};
+
+				await SecureStore.setItemAsync(
+					'hospitalDetails',
+					JSON.stringify(newHospitalDetails)
+				);
+				setHospitalDetails(newHospitalDetails);
+			}
+		} catch (err) {
+			if (!axios.isCancel(err)) {
+				setAppState((prev) => ({
+					...prev,
+					error: true,
+				}));
+			}
+		}
+	}, [getCurrentTimeSpan]);
+
+	const initializeDevice = useCallback(async () => {
+		try {
+			const [deviceIdResult, hospitalResult] = await Promise.all([
+				SecureStore.getItemAsync('deviceId'),
+				SecureStore.getItemAsync('hospitalDetails'),
+			]);
+
+			if (hospitalResult) {
+				setHospitalDetails(JSON.parse(hospitalResult));
+			}
+
+			const id =
+				deviceIdResult ||
+				Math.floor(Math.random() * 899999 + 100000).toString();
+			if (!deviceIdResult) {
+				await SecureStore.setItemAsync('deviceId', id);
+			}
+
+			setDeviceId(id);
+			deviceIdRef.current = id;
+			fetchData();
+		} catch (e) {
+			setAppState((prev) => ({
+				...prev,
+				error: true,
+			}));
+		}
+	}, [fetchData]);
+
+	useEffect(() => {
+		initializeDevice();
+
+		return () => {
+			if (intervalRef.current) clearInterval(intervalRef.current);
+			if (abortControllerRef.current) abortControllerRef.current.abort();
+		};
+	}, [initializeDevice]);
+
+	useEffect(() => {
+		if (!deviceId) return;
+
+		if (intervalRef.current) {
+			clearInterval(intervalRef.current);
+		}
+
+		// intervalRef.current = setInterval(fetchData, 60000);
+		intervalRef.current = setInterval(fetchData, 10000);
+
+		return () => {
+			if (intervalRef.current) clearInterval(intervalRef.current);
+		};
+	}, [deviceId, fetchData]);
+
+	useEffect(() => {
+		if (!deviceId) return;
+
+		Alert.alert(
+			`${
+				process.env.EXPO_PUBLIC_BASE_URL
+			}?deviceCode=${deviceId}&currentTime=${getCurrentTimeSpan()}`
+		);
+	}, [deviceId]);
+
+	const renderDigitBoxes = useMemo(
+		() =>
+			deviceId?.split('').map((digit, index) => (
+				<View
+					key={index}
+					style={styles.digitBox}>
+					<Text style={styles.digit}>{digit}</Text>
+				</View>
+			)),
+		[deviceId]
+	);
+
+	const errorText = useMemo(
+		() =>
+			`Email: ${hospitalDetails?.helpEmail || ''} | Phone: ${
+				hospitalDetails?.helpPhone || ''
+			}`,
+		[hospitalDetails?.helpEmail, hospitalDetails?.helpPhone]
+	);
+
+	if (appState.defaultScreen) {
+		return (
+			<View style={styles.defaultImageContainer}>
+				<Image
+					source={require('../assets/images/default.jpeg')}
+					style={styles.defaultImage}></Image>
+			</View>
+		);
+	}
+
+	if (!appState.connected && !appState.error && !appState.defaultScreen) {
+		return (
+			<View style={styles.centralizedContainer}>
+				<Text
+					style={[
+						styles.optSubtitle,
+						{ position: 'absolute', top: 40, left: 40 },
+					]}>
+					{hospitalDetails?.hospitalName}
+				</Text>
+				<Text style={styles.optTitle}>Enter code</Text>
+				<Text style={styles.optSubtitle}>
+					Enter this code in the back office to connect the device
+				</Text>
+				<View style={styles.codeContainer}>{renderDigitBoxes}</View>
+			</View>
+		);
+	}
+
+	if (appState.error && !doctorData?.doctorName) {
+		return (
+			<View style={styles.errorContainer}>
+				<Text
+					style={[
+						styles.optSubtitle,
+						{ position: 'absolute', top: 40, left: 40 },
+					]}>
+					{hospitalDetails?.hospitalName}
+				</Text>
+				<Text style={styles.optTitle}>Internal Server Error</Text>
+				<Text
+					style={{
+						fontSize: 24,
+						marginBottom: 16,
+						fontFamily: 'InterRegular',
+						color: '#fff',
+					}}>
+					Please Contact Administrator
+				</Text>
+				<Text
+					style={{
+						fontSize: 18,
+						marginBottom: 16,
+						fontFamily: 'InterLight',
+						color: '#fff',
+					}}>
+					{errorText}
+				</Text>
+			</View>
+		);
+	}
+
+	if (!doctorData?.doctorName) {
+		return (
+			<View style={styles.centralizedContainer}>
+				<Text
+					style={[
+						styles.optSubtitle,
+						{ position: 'absolute', top: 40, left: 40 },
+					]}>
+					{hospitalDetails?.hospitalName}
+				</Text>
+				<Text style={styles.optTitle}>Screen is empty</Text>
+			</View>
+		);
+	}
+	return (
+		<View style={styles.container}>
+			<View
+				style={[styles.leftSection, getContainerStyle(doctorData?.bgColor)]}>
+				<Image
+					source={require('../assets/images/line.png')}
+					style={styles.bgImage}></Image>
+				<View
+					style={{
+						flex: 1,
+						flexDirection: 'column',
+						justifyContent: 'space-between',
+						padding: 40,
+						paddingRight: 60,
+					}}>
+					<View>
+						<Text style={styles.lightText}>
+							{hospitalDetails?.hospitalName}
+						</Text>
+					</View>
+					<View>
+						<Text
+							style={{
+								fontSize: 48,
+								fontWeight: 'bold',
+								fontFamily: 'InterBold',
+								color: '#fff',
+								paddingBottom: 10,
+							}}>
+							{doctorData.doctorName}
+						</Text>
+						<Text style={[styles.lightText, { paddingBottom: 20 }]}>
+							{doctorData.department}
+						</Text>
+						<View
+							style={{
+								flexDirection: 'row',
+								borderTopColor: '#3FD2FF',
+								borderTopWidth: 1,
+								alignItems: 'center',
+							}}>
+							<View
+								style={{
+									backgroundColor: '#3FD2FF',
+									paddingHorizontal: 20,
+									paddingVertical: 10,
+									justifyContent: 'center',
+								}}>
+								<Text
+									style={{
+										color: '#fff',
+										fontFamily: 'Inter',
+										textAlign: 'center',
+										fontSize: 24,
+									}}>
+									Doctor in
+								</Text>
+							</View>
+							<Text
+								style={{
+									color: '#fff',
+									fontFamily: 'Inter',
+									textAlign: 'right',
+									flex: 1,
+									fontSize: 24,
+								}}>
+								{doctorData.timing}
+							</Text>
+						</View>
+						<Text
+							style={{
+								fontFamily: 'Inter',
+								color: '#fff',
+								paddingTop: 30,
+								lineHeight: 24,
+								letterSpacing: 0.5,
+							}}>
+							{hospitalDetails?.hospitalWebSite || ''}
+						</Text>
+					</View>
+				</View>
+			</View>
+			<View style={styles.rightSection}>
+				<Image
+					source={{ uri: doctorData?.photoUrl || '' }}
+					style={styles.image}
+				/>
+			</View>
+		</View>
+	);
+}
